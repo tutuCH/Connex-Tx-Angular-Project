b436e8bbba98c136f44bfce44f159179c3973dae
tutuCH <tuchenhsien@gmail.com>
2023-07-17 11:11:28 -0400

    update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: 'http://44.208.180.210/api',
+    API_URL: 'https://xmgu152kkh.execute-api.us-east-1.amazonaws.com/default/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
     API_URL: 'http://localhost:8080/api'
-    // API_URL: 'http://44.208.180.210/api',
 };
