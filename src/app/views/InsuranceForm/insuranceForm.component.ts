139675db76fc99ee642e51cbaf6940793f88c779
tutuCH <tuchenhsien@gmail.com>
2023-07-16 23:18:28 -0400

    add number validation rule

--- src/app/views/InsuranceForm/insuranceForm.component.ts
+++ src/app/views/InsuranceForm/insuranceForm.component.ts
@@ -5,7 +5,7 @@ import { MyErrorStateMatcher } from '../../utils/ErrorMatcher/errorMatcher.compo
 import { Router } from '@angular/router';
 import { FormDataService } from 'src/app/utils/FormDataServices/formDataServices';
 import { FormDataKey } from 'src/app/data/dataTypes';
-import { InsuranceFormQuestionList } from '../../data/data'
+
 // Define the component
 @Component({
   selector: 'insuranceForm',
@@ -14,6 +14,43 @@ import { InsuranceFormQuestionList } from '../../data/data'
 })
 export class InsuranceFormComponent {
   // Define an array of objects that contains information about each form control (currently commented out)
+  insuranceFormQuestionList = [
+    {
+      formControlName: "ageFormControl",
+      label: "What is your age?",
+      placeholder: "age",
+    },
+    {
+      formControlName: "drivingExperienceFormControl",
+      label: "What is your driving experience?",
+      placeholder: "driving experience",
+    },
+    {
+      formControlName: "drivingRecordFormControl",
+      label: "No. of times of fault traffic violations in last 5 years",
+      placeholder: "driver record",
+    },
+    {
+      formControlName: "claimsFormControl",
+      label: "How many times have you claimed before?",
+      placeholder: "claims",
+    },
+    {
+      formControlName: "carValueFormControl",
+      label: "How much is your car?",
+      placeholder: "price",
+    },
+    {
+      formControlName: "annualMileageFormControl",
+      label: "What is the annual mileage of your car?",
+      placeholder: "Annual Mileage",
+    },
+    {
+      formControlName: "insuranceHistoryFormControl",
+      label: "What is your insurance history?",
+      placeholder: "insurance history",
+    }
+  ]
   constructor(private router: Router, private formDataService: FormDataService) { }
   ngOnInit() { 
     const completedInsuranceForm = this.formDataService.getFormData(FormDataKey.INSURANCE_FORM);
@@ -21,17 +58,17 @@ export class InsuranceFormComponent {
       this.insuranceForm.setValue(completedInsuranceForm.value);
     }
   }
-  insuranceFormQuestionList = InsuranceFormQuestionList;
-  insuranceForm = new FormGroup({
-    ageFormControl: new FormControl(null, [Validators.required, Validators.min(18)]),
-    drivingExperienceFormControl: new FormControl(null, [Validators.required, Validators.min(0)]),
-    driverRecordFormControl: new FormControl(null, [Validators.required, Validators.min(0)]),
-    claimsFormControl: new FormControl(null, [Validators.required, Validators.min(0)]),
-    carValueFormControl: new FormControl(null, [Validators.required, Validators.min(0)]),
-    annualMileageFormControl: new FormControl(null, [Validators.required, Validators.min(0)]),
-    insuranceHistoryFormControl: new FormControl(null, [Validators.required, Validators.min(0)]),
-  });
 
+  // Define the form group with its form controls and validators
+  insuranceForm = new FormGroup({
+    ageFormControl: new FormControl(null, [Validators.required, Validators.min(18), Validators.pattern(/^[0-9]*$/)]),
+    drivingExperienceFormControl: new FormControl(null, [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]*$/)]),
+    driverRecordFormControl: new FormControl(null, [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]*$/)]),
+    claimsFormControl: new FormControl(null, [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]*$/)]),
+    carValueFormControl: new FormControl(null, [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]*$/)]),
+    annualMileageFormControl: new FormControl(null, [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]*$/)]),
+    insuranceHistoryFormControl: new FormControl(null, [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]*$/)]),
+  });
 
   // Define an error matcher to use with the form controls
   errorMatcher = new MyErrorStateMatcher();
